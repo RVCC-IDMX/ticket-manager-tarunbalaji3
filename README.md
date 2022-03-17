28235a4a0744dbebdc761b404037637ad62b7f18
Tarun Balaji Ravi <tarunbalaji3@gmail.com>
2022-03-16 20:57:49 -0400

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# template
+# Ticket Manager
+
+## App Behavior
+
+This app is a ticket manager that allows users to buy tickets. It uses event emitters to trigger events, and event listeners to "listen" and execute code when the events are trigged. In addition, the app also manages errors from the event emitter, such as buying too many tickets. The chalk module adds colorful styling to the terminal interface.
+
+### Instructions
+
+In the command line, enter npm run watch.
+
+![command line screenshot](/images/img_1.png)
+
+The following output should pop up on your screen:
+
+![terminal screenshot](/images/img_2.png)
+
+If the tickets run out, the following error message appears:
+
+![terminal screenshot](/images/img_3.png)
--- index.js
+++ index.js
@@ -1,39 +1,79 @@
 const chalk = require('chalk');
 
-const TicketManager = require("./ticketManager");
-const EmailService = require("./emailService");
-const DatabaseService = require("./databaseService");
+const TicketManager = require('./ticketManager');
+const EmailService = require('./emailService');
+const DatabaseService = require('./databaseService');
 
 const ticketManager = new TicketManager(3);
 const emailService = new EmailService();
 const databaseService = new DatabaseService();
 
-ticketManager.on("buy", (email, price, timestamp) => {
-    emailService.send(email);
-    databaseService.save(email, price, timestamp);
+ticketManager.on('buy', (email, price, timestamp) => {
+  emailService.send(email);
+  databaseService.save(email, price, timestamp);
 });
 
-ticketManager.on("error", (error) => {
-    console.error(`Gracefully handling our error: ${error}`);
+ticketManager.on('error', (error) => {
+  console.error(`Gracefully handling our error: ${error}`);
 });
 
-console.log(chalk.hex('#00FFFF').bold(`We have ${ticketManager.listenerCount("buy")} listener(s) for the buy event`));
-console.log(chalk.hex('#FF00FF').bold(`We have ${ticketManager.listenerCount("error")} listener(s) for the error event`));
+console.log(
+  chalk
+    .hex('#00FFFF')
+    .bold(
+      `We have ${ticketManager.listenerCount(
+        'buy'
+      )} listener(s) for the buy event`
+    )
+);
+console.log(
+  chalk
+    .hex('#FF00FF')
+    .bold(
+      `We have ${ticketManager.listenerCount(
+        'error'
+      )} listener(s) for the error event`
+    )
+);
 
 const onBuy = () => {
-    console.log(chalk.hex('#39FF14').bold("I will be removed soon"));
+  console.log(chalk.hex('#39FF14').bold('I will be removed soon'));
 };
-ticketManager.on("buy", onBuy);
+ticketManager.on('buy', onBuy);
 
-console.log(chalk.hex('#FFFFFF').bold(`We added a new event listener bringing our total count for the buy event to: ${ticketManager.listenerCount("buy")}`));
-ticketManager.buy("test@email", 20);
+console.log(
+  chalk
+    .hex('#FFFFFF')
+    .bold(
+      `We added a new event listener bringing our total count for the buy event to: ${ticketManager.listenerCount(
+        'buy'
+      )}`
+    )
+);
+ticketManager.buy('test@email', 20);
 
-ticketManager.off("buy", onBuy);
+ticketManager.off('buy', onBuy);
 
-console.log(chalk.hex('#00FFFF').bold(`We now have: ${ticketManager.listenerCount("buy")} listener(s) for the buy event`));
-ticketManager.buy("test@email", 20);
+console.log(
+  chalk
+    .hex('#00FFFF')
+    .bold(
+      `We now have: ${ticketManager.listenerCount(
+        'buy'
+      )} listener(s) for the buy event`
+    )
+);
+ticketManager.buy('test@email', 20);
 
-ticketManager.removeAllListeners("buy");
-console.log(chalk.hex('#FF00FF').bold(`We have ${ticketManager.listenerCount("buy")} listeners for the buy event`));
-ticketManager.buy("test@email", 20);
-console.log(chalk.hex('#39FF14').bold("The last ticket was bought"));
+ticketManager.removeAllListeners('buy');
+console.log(
+  chalk
+    .hex('#FF00FF')
+    .bold(
+      `We have ${ticketManager.listenerCount(
+        'buy'
+      )} listeners for the buy event`
+    )
+);
+ticketManager.buy('test@email', 20);
+console.log(chalk.hex('#39FF14').bold('The last ticket was bought'));
